608eb99189369e8ae9419151133f9f7b2599c91c
rbsec <robin@rbsec.net>
2013-06-01 14:41:50 +0100

    Added genwordlist.py

---
+++ genwordlist.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#
+# Simple script to generate a sorted unique wordlist from an input file
+# Usage: $ ./genwordlist.py <inputfile>
+# Saves output to wordlist.txt
+#
+import re
+import sys
+
+wordlist = []
+with open(sys.argv[1]) as f:
+    lines = f.readlines()
+for line in lines:
+    words = re.findall("[a-zA-Z0-9\-]+", line)
+    for word in words:
+        wordlist.append(word)
+
+wordlist = sorted(set(wordlist), key=lambda s: s.lower())
+f = open("wordlist.txt", "w")
+for word in wordlist:
+    f.write(word + "\n")
+print("Wrote " + str(len(wordlist)) + " words to wordlist.txt")
