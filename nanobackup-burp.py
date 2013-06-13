03af4374982d662a4fc1e5ce79d92d924a1aad1e
rbsec <robin@rbsec.net>
2013-06-13 15:36:29 +0100

    Added nano backup file generator

---
+++ nanobackup-burp.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+#
+# Simple script to generate a list of nano backup files (.php~) from a burp sitemap
+# Usage: $ ./nanobackup-burp.py <inputfile> [outfile]
+# Saves output to wordlist.txt by default
+#
+import base64
+import re
+import sys
+try:    # Use faster C implementation if we can
+    import xml.etree.cElementTree as ET
+except ImportError:
+    import xml.etree.ElementTree as ET
+
+wordlist = set()
+
+for event, elem in ET.iterparse(sys.argv[1]):
+    if event == 'end':
+        if elem.tag == 'url':
+            url = str(elem.text)
+            try:
+                file = re.search("https?://[a-zA-Z0-9\-\.]+(.*?\.php)", url).group(1)
+                wordlist.add(file)
+            except:
+                pass
+    elem.clear() # Discard the element to free memory
+
+
+wordlist = sorted(wordlist, key=lambda s: s.lower())    # Case insensitive sort
+
+try:
+    outfile = sys.argv[2]
+except IndexError:
+    outfile = "wordlist.txt"
+f = open(outfile, "w")
+for word in wordlist:
+    f.write(word + "~\n")
+print("Wrote " + str(len(wordlist)) + " words to " + outfile)
