653805ba86ab5ff7b1d536f7100d4803eb63d720
rbsec <robin@rbsec.net>
2019-05-29 09:23:37 +0100

    Remove some legacy code

--- vfinder.py
+++ vfinder.py
@@ -21,13 +21,6 @@ def md5(fname):
             hash_md5.update(chunk)
     return hash_md5.hexdigest()
 
-def sha1(fname):
-    hash_sha1 = hashlib.sha1()
-    with open(fname, "rb") as f:
-        for chunk in iter(lambda: f.read(4096), b""):
-            hash_sha1.update(chunk)
-    return hash_sha1.hexdigest()
-
 if len(sys.argv) < 3:
     print("Usage: $ ./vfinder.py <repo> <filepath>")
     sys.exit(1)
